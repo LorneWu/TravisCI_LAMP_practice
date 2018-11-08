568ed62d701950730b40e7ece48865446e851801
Wu Wen Chun <LorneWu@users.noreply.github.com>
2018-11-08 16:17:13 +0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -5,10 +5,5 @@ import os
 chrome_options = Options()  
 chrome_options.add_argument("--headless")  
 chrome_options.binary_location = '/usr/local/bin'    
-try:
-  driver = webdriver.Chrome(chrome_options=chrome_options)
-except Exception as e:
-  print("fail")
-  print(e)
 
-driver =webdriver.Chrome(executable_path=os.path.abspath("chromedriver"),chrome_options=chrome_options)  
+driver =webdriver.Chrome(chrome_options=chrome_options)  
