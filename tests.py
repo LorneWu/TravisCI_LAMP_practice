e0a4a6e24cf20baa5939b82d9a3024967455e4e9
Wu Wen Chun <LorneWu@users.noreply.github.com>
2018-11-08 15:44:33 +0800

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,4 +1,8 @@
-from selenium import webdriver
- 
-browser = webdriver.Chrome("/usr/local/bin/chromedriver")
-browser.get('http://www.baidu.com/')
+from selenium import webdriver 
+from selenium.webdriver.chrome.options import Options 
+
+chrome_options = Options()  
+chrome_options.add_argument("--headless")  
+chrome_options.binary_location = '/usr/local/share/chromedriver'    
+
+driver = webdriver.Chrome(executable_path=os.path.abspath(“chromedriver"),chrome_options=chrome_options)  
